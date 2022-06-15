acd4bcd49cb1bafcd67c51c395f130c997cd7703
reyhane <reyhane.hosseinzade78@gmail.com>
2022-06-15 11:35:32 +0430

    add podcast2

---
+++ podcast2/js/app.js
@@ -0,0 +1,77 @@
+$(function () {
+    // menu-bar*******************************
+    $(".ham-icon-menu").click(function () {
+      $(".menu-mobile").addClass("active-menu-mobile");
+      $(".beforemain").css("width", "100%");
+    });
+    $(".beforemain").click(function () {
+      $(".menu-mobile").removeClass("active-menu-mobile");
+      $(".beforemain").css("width", "0");
+    });
+    // open search box**************************
+    $(".open-search").click(function () {
+      $(".search-input").css("display", "block");
+    });
+    // close search box***************************
+    $(".close-search").click(function () {
+      $(".search-input").css("display", "none");
+    });
+    // footer link*******************
+    $(".toggle-icon1").click(function () {
+      $(".peyvandha-links").slideToggle();
+    });
+    $(".toggle-icon2").click(function () {
+      $(".questions-1-links").slideToggle();
+    });
+    $(".toggle-icon3").click(function () {
+      $(".questions-2-links").slideToggle();
+    });
+  });
+
+  // close menu bar ****************
+  function closeMenuBar() {
+    $(".menu-mobile").removeClass("active-menu-mobile");
+    $(".beforemain").css("width", "0");
+  }
+  
+  // countDown****************
+  const countdown = document.querySelector(".countdown");
+  const dayDisp = document.getElementById("day");
+  const hourDisp = document.getElementById("hour");
+  const minuteDisp = document.getElementById("minute");
+  const secondDisp = document.getElementById("second");
+  let countDownEnd = 1658172599000;
+  let interval = setInterval(() => {
+    let now = new Date().getTime();
+    let timeDifference = countDownEnd - now;
+    let dayLeft = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    let hourLeft = Math.floor(
+      (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minuteLeft = Math.floor(
+      (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    let secondLeft = Math.floor((timeDifference % (1000 * 60)) / 1000);
+  
+    dayLeft < 10
+      ? (dayDisp.innerHTML = "0" + dayLeft)
+      : (dayDisp.innerHTML = dayLeft);
+  
+    hourLeft < 10
+      ? (hourDisp.innerHTML = "0" + hourLeft)
+      : (hourDisp.innerHTML = hourLeft);
+  
+    minuteLeft < 10
+      ? (minuteDisp.innerHTML = "0" + minuteLeft)
+      : (minuteDisp.innerHTML = minuteLeft);
+  
+    secondLeft < 10
+      ? (secondDisp.innerHTML = "0" + secondLeft)
+      : (secondDisp.innerHTML = secondLeft);
+  
+    if (timeDifference < 0) {
+      clearInterval(interval);
+      countdown.innerHTML = "end";
+    }
+  }, 1000);
+  
